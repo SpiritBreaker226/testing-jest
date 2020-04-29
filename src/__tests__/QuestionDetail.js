241ae6afe8821c292c61c7a012c84e326a53ed94
Jason Stathopulos <jstathopulos@gmail.com>
2020-04-29 18:27:29 -0400

    Add Example of Component Testing For Stateless
    Add an example of component testing for stateless components that show
    how to test both functionally and render parts of a component.

--- src/__tests__/QuestionDetail.js
+++ src/__tests__/QuestionDetail.js
@@ -1,3 +1,40 @@
+import React from 'react'
+import TestRenderer from 'react-test-renderer'
+
+import {
+  mapStateToProps,
+  QuestionDetailDisplay
+} from '../components/QuestionDetail'
+
 describe('The Question Detail Component', () => {
-  it('should not regress', () => {})
+  describe('for container element', () => {
+    describe('mapStateToProps', () => {
+      it('should map the state to props correctly', () => {
+        const sampleQuestion = {
+          question_id: 42,
+          body: 'Space is big',
+        }
+        const appState = { questions: [sampleQuestion] }
+        const ownProps = { question_id: 42 }
+        const componentState = mapStateToProps(appState, ownProps)
+
+        expect(componentState).toEqual(sampleQuestion)
+      })
+    })
+  })
+
+  describe('display element', () => {
+    it('should not regress', () => {
+      const tree = TestRenderer.create(
+        <QuestionDetailDisplay
+          title="The meaning of life"
+          body="42"
+          answer_count={0}
+          tags={['hitchhiking']}
+        />
+      )
+
+      expect(tree.toJSON()).toMatchSnapshot()
+    })
+  })
 })
